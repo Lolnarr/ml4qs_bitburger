0877c67b817a11f32c5c44964c897623e394ef85
lennart <lolnarrlps@gmail.com>
2021-06-05 20:16:59 +0200

    show data automated

--- show_data.py
+++ show_data.py
@@ -1,23 +1,27 @@
+import time
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def main():
+def main(letter):
     # Read in data using pandas
-    file = input("Dateiname (xyz.csv): ")
-    data = pd.read_csv(f'split_data/A/{file}.csv', sep=',', decimal='.')
+    for i in range(21):
+        # file = input("Dateiname (xyz.csv): ")
+        data = pd.read_csv(f'split_data/{letter}/{letter}_{i+1}.csv', sep=',', decimal='.')
 
-    fig, axs = plt.subplots(2, sharex=True, sharey=False)
-    axs[0].plot(data['time'], data['accX'], label='X-axis')
-    axs[0].plot(data['time'], data['accY'], label='Y-axis')
-    axs[0].plot(data['time'], data['accZ'], label='Z-axis')
-    axs[0].set(title='Accelerometer data', xlim=(0, data['time'].max()))
-    axs[1].plot(data['time'], data['gyrX'], label='X-axis')
-    axs[1].plot(data['time'], data['gyrY'], label='Y-axis')
-    axs[1].plot(data['time'], data['gyrZ'], label='Z-axis')
-    axs[1].set(title='Gyroscope data', xlim=(0, data['time'].max()))
-    plt.legend()
-#    plt.savefig(f'recorded_data/{file}.png')
-    plt.show()
+        fig, axs = plt.subplots(2, sharex=True, sharey=False)
+        axs[0].plot(data['time'], data['accX'], label='X-axis')
+        axs[0].plot(data['time'], data['accY'], label='Y-axis')
+        axs[0].plot(data['time'], data['accZ'], label='Z-axis')
+        axs[0].set(title='Accelerometer data', xlim=(0, data['time'].max()))
+        axs[1].plot(data['time'], data['gyrX'], label='X-axis')
+        axs[1].plot(data['time'], data['gyrY'], label='Y-axis')
+        axs[1].plot(data['time'], data['gyrZ'], label='Z-axis')
+        axs[1].set(title='Gyroscope data', xlim=(0, data['time'].max()))
+        plt.legend()
+    #    plt.savefig(f'recorded_data/{file}.png')
+        plt.show()
+        time.sleep(1)
 
 
 # Plotting the axes in individual plots
@@ -31,4 +35,4 @@ plt.show()
 
 if __name__ == "__main__":
     while True:
-        main()
+        main(input("Buchstabe? ").upper())
