400d5a35ab113bb529507abd964ffdc7dfb5e071
lennart <lolnarrlps@gmail.com>
2021-06-16 16:31:15 +0200

    show data automated

--- ml.py
+++ ml.py
@@ -4,3 +4,28 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 # https://www.tensorflow.org/guide/keras/rnn
+
+DATA_PATH = 'split_data'
+
+
+def load_data(path) -> Tuple[np.ndarray, np.ndarray]:
+
+    for foldername in sorted(os.listdir(path)):
+        for filename in sorted(os.listdir(f'{path}/{foldername}')):
+            df = pd.read_csv(f'{path}/{foldername}/{filename}')
+            array = df.to_numpy()
+            fill_len = 1000 - array.shape[0]
+            full_array = np.full((fill_len, array.shape[1]), 0)
+            array = np.concatenate((array, full_array))
+            print(array.shape)
+            for row in array:
+                print(row)
+
+
+
+def main():
+    load_data(DATA_PATH)
+
+
+if __name__ == '__main__':
+    main()
