e303e9c09412df24dd4a7880c7058285d8bf4978
LennartN <lennart.nitz@gmx.de>
2021-07-02 19:43:31 +0200

    Augmentation now compatible with roll, pitch and yaw data

--- augmentation_program.py
+++ augmentation_program.py
@@ -8,7 +8,7 @@ TEST_PATH = 'normalized_data/test'
 VAL_PATH = 'normalized_data/validation'
 
 
-def add_gyronoise(df):
+def add_gyronoise(df: pd.DataFrame):
     mu, sigma = 0, 1.0
     noise = np.random.normal(mu, sigma, [500, 3])
     fusion = df[['gyrX', 'gyrY', 'gyrZ']] + noise
@@ -18,20 +18,20 @@ def add_gyronoise(df):
     return df
 
 
-def add_noise(df):
+def add_noise(df: pd.DataFrame):
     mu, sigma = 0, 1.0
-    noise = np.random.normal(mu, sigma, [500, 6])
-    fusion = df + noise
+    noise = np.random.normal(mu, sigma, [500, 3])
+    fusion = df[['roll', 'pitch', 'yaw']] + noise
     return fusion
 
 
-def stretch_data(df):
-    accX, accY, accZ, gyrX, gyrY, gyrZ = np.random.randn(6) * 0.3 + 1
-    df = df * np.array([accX, accY, accZ, gyrX, gyrY, gyrZ])
+def stretch_data(df: pd.DataFrame):
+    r, p, y = np.random.randn(3) * 0.3 + 1
+    df = df[['roll', 'pitch', 'yaw']] * np.array([r, p, y])
     return df
 
 
-def stretch_gyrodata(df):
+def stretch_gyrodata(df: pd.DataFrame):
     gyrX, gyrY, gyrZ = np.random.randn(3) * 0.3 + 1
     df_new = df[['gyrX', 'gyrY', 'gyrZ']] * np.array([gyrX, gyrY, gyrZ])
     df['gyrX'] = df_new['gyrX']
@@ -40,7 +40,7 @@ def stretch_gyrodata(df):
     return df
 
 
-def rotate_data(df):
+def rotate_data(df: pd.DataFrame):
     rotation_axis = np.array([0, 0, 1])
     theta = np.random.randn() * 5
     q = np.hstack([np.array(math.cos(np.radians(theta / 2.0))),
@@ -49,22 +49,15 @@ def rotate_data(df):
     df_new = np.matmul(np.array([[a ** 2 + b ** 2 - c ** 2 - d ** 2, 2 * b * c - 2 * a * d, 2 * b * d + 2 * a * c],
                                  [2 * b * c + 2 * a * d, a ** 2 - b ** 2 + c ** 2 - d ** 2, 2 * c * d - 2 * a * b],
                                  [2 * b * d - 2 * a * c, 2 * c * d + 2 * a * b, a ** 2 - b ** 2 - c ** 2 + d ** 2]]),
-                       df[['gyrX', 'gyrY', 'gyrZ']].T).T
-    df_new.columns = ['gyrX', 'gyrY', 'gyrZ']
-    df['gyrX'] = df_new['gyrX']
-    df['gyrY'] = df_new['gyrY']
-    df['gyrZ'] = df_new['gyrZ']
-    return df
+                       df[['roll', 'pitch', 'yaw']].T).T
+    df_new.columns = ['roll', 'pitch', 'yaw']
+    # df['gyrX'] = df_new['gyrX']
+    # df['gyrY'] = df_new['gyrY']
+    # df['gyrZ'] = df_new['gyrZ']
+    return df_new
 
 
-# def quaternion_to_rotation_matrix(q):
-#     a, b, c, d = q
-#     return np.array([[a ** 2 + b ** 2 - c ** 2 - d ** 2, 2 * b * c - 2 * a * d, 2 * b * d + 2 * a * c],
-#                      [2 * b * c + 2 * a * d, a ** 2 - b ** 2 + c ** 2 - d ** 2, 2 * c * d - 2 * a * b],
-#                      [2 * b * d - 2 * a * c, 2 * c * d + 2 * a * b, a ** 2 - b ** 2 - c ** 2 + d ** 2]])
-
-
-def start_augmentation(path: str, rotate=True, gyronoise=True, noise=False, gyrostretch=True, stretch=False):
+def start_augmentation(path: str, rotate=True, gyronoise=False, noise=True, gyrostretch=False, stretch=True):
     for foldername in os.listdir(path):
         if os.path.basename(path) == 'training':
             datafolder = 'training'
